804a2e5d85085e51574fe208efffd19983542a84
Aimroidwrld <263405274+Aimroidwrld@users.noreply.github.com>
2026-04-02 23:44:39 +0100

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -8,7 +8,7 @@ function generate_captcha_question() {
     $a = rand(1, 9);
     $b = rand(1, 9);
     $_SESSION['captcha_answer'] = $a + $b;
-    return "What is $a + $b ?";
+    return "What is $a + $b?";
 }
 
 function check_captcha_answer($answer) {
